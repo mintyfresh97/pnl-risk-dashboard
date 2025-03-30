3c9758e34f544236c288f63943917f7a848a0c7b
mintyfresh97 <94802870+mintyfresh97@users.noreply.github.com>
2025-03-30 16:36:38 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="PnL Risk Dashboard", layout="centered")
+
+st.title("Live PnL + Risk Calculator")
+
+# Later you’ll embed the custom component here
+st.info("React component will load here...")
