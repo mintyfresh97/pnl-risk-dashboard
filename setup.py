3063444815a70e79dd53a649588c31d901b50263
mintyfresh97 <94802870+mintyfresh97@users.noreply.github.com>
2025-03-30 16:37:01 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pnl_risk_dashboard",
+    version="0.1.0",
+    packages=["my_component"],
+    include_package_data=True,
+    install_requires=[],
+)
