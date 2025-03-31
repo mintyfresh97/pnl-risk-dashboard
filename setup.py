f5bab89ee4ee3a0e2c32e303438221a75b0dcdcb
mintyfresh97 <94802870+mintyfresh97@users.noreply.github.com>
2025-03-31 14:22:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="pnl_risk_dashboard",
     version="0.1.0",
-    packages=["my_component"],
+    packages=["frontend.my_component"],
     include_package_data=True,
     install_requires=[],
 )
